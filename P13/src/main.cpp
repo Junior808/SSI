d419fa670d9956df060d729e45959be4356eb57b
Damián Eduardo Domínguez <alu0101138183@ull.edu.es>
2020-05-13 19:49:26 +0100

    Añadido elgamal

--- P13/src/main.cpp
+++ P13/src/main.cpp
@@ -6,6 +6,7 @@
 #include "AES.hpp"
 #include "AES_CBC.hpp"
 #include "RSA.hpp"
+#include "ElGamalEliptico.hpp"
 
 #include <iostream>
 #include <ctime>
@@ -1028,6 +1029,37 @@ void rsa()
 
 void elgamal()
 {
+    int primo, a, b, x, y, aA, dB, mensaje;
+
+    std::cout << "\nIntroducir número primo p: ";
+    std::cin >> primo;
+    std::cout << "\nCurva elíptica y^2 = x^3 + ax + b.";
+    std::cout << "\nIntroducir valor de a: ";
+    std::cin >> a;
+    std::cout << "Introducir valor de b: ";
+    std::cin >> b;
+
+    ElGamalEliptico cifrado(a, b, primo);
+
+    std::cout << "\nIndique el punto base.\n";
+    std::cout << "\t- Indique la coordenada X: ";
+    std::cin >> x;
+    std::cout << "\t- Indique la coordenada y: ";
+    std::cin >> y;
+
+    std::cout << "\nIndique la clave privada aA: ";
+    std::cin >> aA;
+
+    std::cout << "Indique la clave privada dB: ";
+    std::cin >> dB;
+
+    cifrado.set_claves(aA, dB);
+
+    std::cout << "\nIntroduzca el mensaje a cifrar: ";
+    std::cin >> mensaje;
+
+    cifrado.cifrar(mensaje, std::make_pair(x, y));
+    cifrado.write();
 }
 
 int main()
